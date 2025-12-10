49882cb71342d912c32a44e0995547d2a24c1a93
JCS-Mecabricks <maestronv@gmail.com>
2025-12-10 14:10:05 -0600

    Updated to 1.21.11

--- src/main/java/jcsmecabricks/individualkeepinv/IKeepInvCommand.java
+++ src/main/java/jcsmecabricks/individualkeepinv/IKeepInvCommand.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.permission.Permission;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -17,15 +18,15 @@ public class IKeepInvCommand {
     public static void commandLogic (CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(literal("ikeepinv")
 
-                .then(CommandManager.literal("getdefault")
-                        .requires (source -> source.hasPermissionLevel(2))
+                .then(literal("getdefault")
+                        .requires (source -> source.getPermissions().hasPermission(Permission.Atom.ofVanilla("command/level/2")))
                                 .executes(ctx -> {
                                     ctx.getSource().sendMessage(Text.of("The current default state is: " + kim.keepInvDefault));
                                     return 1;
                                 }))
 
-                .then(CommandManager.literal("default")
-                        .requires (source -> source.hasPermissionLevel(2))
+                .then(literal("default")
+                        .requires (source -> source.getPermissions().hasPermission(Permission.Atom.ofVanilla("command/level/2")))
                         .then(CommandManager.argument("boolean", BoolArgumentType.bool())
                                 .executes(ctx -> {
                                     boolean bool = BoolArgumentType.getBool(ctx, "boolean");
@@ -34,12 +35,12 @@ public class IKeepInvCommand {
                                     return 1;
                                 })))
 
-            .then(CommandManager.literal("get")
+            .then(literal("get")
                     .then(CommandManager.argument("target", EntityArgumentType.player())
-                            .requires(source -> source.hasPermissionLevel(0))
+                            .requires(source -> source.getPermissions().hasPermission(Permission.Atom.ofVanilla("command/level/0")))
                             .executes(ctx -> {
                                 ServerPlayerEntity player = EntityArgumentType.getPlayer(ctx, "target");
-                                if (ctx.getSource().hasPermissionLevel(2)) {  // hasPermissionLevel also works for permission levels above specified, so this works for permission level 4 as well
+                                if (ctx.getSource().getPermissions().hasPermission(Permission.Atom.ofVanilla("command/level/2"))) {  // hasPermissionLevel also works for permission levels above specified, so this works for permission level 4 as well
                                     ctx.getSource().sendMessage(Text.of(player.getName().getString() + "'s inventory state is currently: " + KeepInvMap.getPlayerState(player)));
                                 }
                                 else if (player.equals(ctx.getSource().getPlayer())) { // checks if player executing command is the same as the player passed to the command
@@ -51,13 +52,13 @@ public class IKeepInvCommand {
                                 return 1;
                             })))
 
-            .then(CommandManager.literal("set")
+            .then(literal("set")
                     .then(CommandManager.argument("target", EntityArgumentType.player())
                             .then(CommandManager.argument("boolean", BoolArgumentType.bool())
                                     .executes(ctx -> {
                                         ServerPlayerEntity player = EntityArgumentType.getPlayer(ctx, "target");
                                         boolean bool = BoolArgumentType.getBool(ctx, "boolean");
-                                        if (ctx.getSource().hasPermissionLevel(2)) {  // hasPermissionLevel also works for permission levels above specified, so this works for permission level 4 as well
+                                        if (ctx.getSource().getPermissions().hasPermission(Permission.Atom.ofVanilla("command/level/2"))) {  // hasPermissionLevel also works for permission levels above specified, so this works for permission level 4 as well
                                             KeepInvMap.setPlayerState(player, bool);
                                             ctx.getSource().sendMessage(Text.of(player.getName().getString() + "'s inventory state has been set to: " + bool));
                                         }
--- src/main/java/jcsmecabricks/individualkeepinv/KeepInvMap.java
+++ src/main/java/jcsmecabricks/individualkeepinv/KeepInvMap.java
@@ -1,30 +1,62 @@
 package jcsmecabricks.individualkeepinv;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.datafixer.DataFixTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.PersistentState;
-import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.PersistentStateType;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static jcsmecabricks.individualkeepinv.IndividualKeepInv.MOD_ID;
 
 public class KeepInvMap extends PersistentState {
-
     public final HashMap<UUID, Boolean> invStateMap = new HashMap<>();
     public boolean keepInvDefault = false;
 
     public static KeepInvMap kim;
+
+    // --- Codec ---
+    public static final Codec<KeepInvMap> CODEC = RecordCodecBuilder.create(instance ->
+            instance.group(
+                    Codec.BOOL.fieldOf("keepInvDefault").forGetter(map -> map.keepInvDefault),
+                    Codec.unboundedMap(Codec.STRING, Codec.BOOL).fieldOf("playerStates").forGetter(map ->
+                            map.invStateMap.entrySet().stream()
+                                    .collect(Collectors.toMap(e -> e.getKey().toString(), Map.Entry::getValue))
+                    )
+            ).apply(instance, (keepInvDefault, playerStates) -> {
+                KeepInvMap map = new KeepInvMap();
+                map.keepInvDefault = keepInvDefault;
+                playerStates.forEach((k, v) -> map.invStateMap.put(UUID.fromString(k), v));
+                return map;
+            })
+    );
+
+    public static final PersistentStateType<KeepInvMap> TYPE = new PersistentStateType<>(MOD_ID, KeepInvMap::new, CODEC, DataFixTypes.PLAYER);
+
+    public KeepInvMap() {}
+
+    // --- Utility methods ---
+    public static KeepInvMap get(ServerWorld world) {
+        kim = world.getPersistentStateManager().getOrCreate(TYPE);
+        return kim;
+    }
+
+    private static void ensureLoaded(PlayerEntity player) {
+        if (kim == null && player.getEntityWorld() instanceof ServerWorld serverWorld) {
+            get(serverWorld);
+        }
+    }
 
     public static boolean getPlayerState(PlayerEntity player) {
         ensureLoaded(player);
@@ -44,57 +76,11 @@ public class KeepInvMap extends PersistentState {
         }
     }
 
-    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup lookup) {
-        NbtCompound playersNbt = new NbtCompound();
-        for (var entry : invStateMap.entrySet()) {
-            NbtCompound playerData = new NbtCompound();
-            playerData.putBoolean("invBool", entry.getValue());
-            playersNbt.put(entry.getKey().toString(), playerData);
-        }
-        nbt.put("invStateCompound", playersNbt);
-        nbt.putBoolean("keepInvDefault", keepInvDefault);
-        return nbt;
-    }
-
-    public static KeepInvMap fromNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup lookup) {
-        KeepInvMap map = new KeepInvMap();
-        NbtCompound compound = nbt.getCompound("invStateCompound").get();
-
-        for (String key : compound.getKeys()) {
-            UUID uuid = UUID.fromString(key);
-            boolean keepInv = compound.getCompound(key).flatMap(nbt1 -> nbt.getBoolean("invBool")).orElse(false);
-            map.invStateMap.put(uuid, keepInv);
-        }
-
-        map.keepInvDefault = nbt.getBoolean("keepInvDefault").get();
-        return map;
-    }
-
-    public static final PersistentStateType<KeepInvMap> TYPE =
-            new PersistentStateType<>(
-                    MOD_ID,
-                    ctx -> new KeepInvMap(),
-                    ctx -> Codec.unit(new KeepInvMap()),
-                    DataFixTypes.PLAYER
-            );
-
-
-    public static KeepInvMap get(ServerWorld world) {
-        return world.getPersistentStateManager().getOrCreate(TYPE);
-    }
-
-
-    private static void ensureLoaded(PlayerEntity player) {
-        if (kim == null && player.getEntityWorld() instanceof ServerWorld serverWorld) {
-            get(serverWorld);
-        }
-    }
-
     public static void onJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
         kim = get(server.getOverworld());
-
-        if (!kim.invStateMap.containsKey(handler.player.getUuid())) {
-            kim.invStateMap.put(handler.player.getUuid(), kim.keepInvDefault);
+        UUID uuid = handler.player.getUuid();
+        if (!kim.invStateMap.containsKey(uuid)) {
+            kim.invStateMap.put(uuid, kim.keepInvDefault);
             kim.markDirty();
         }
     }
